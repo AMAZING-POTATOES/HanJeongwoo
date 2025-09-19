7a11cff586537b17df6de014aa0486f3b17ce3a1
pe66le <jw.haaan@gmail.com>
2025-09-19 15:36:23 +0900

    0919_Practice (2-01 ~ 2-03)

---
+++ src/main/java/com/mysite/sbb/Sbb0919Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb0919Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb0919Application.class, args);
+	}
+
+}
