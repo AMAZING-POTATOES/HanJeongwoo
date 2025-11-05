3ce9168d961ea8fa86c955ad5874c3584fff7b42
pe66le <167800612+pe66le@users.noreply.github.com>
2025-11-06 02:01:00 +0900

    2-09. 컨트롤러에서 서비스 사용

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,19 +1,12 @@
-package com.mysite.sbb.question;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        model.addAttribute("questionList", questionRepository.findAll());
+        model.addAttribute("questionList", questionService.getList());
         return "question_list";
     }
 }
