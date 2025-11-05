017664371dead08530b371dc642d01ae8657f786
pe66le <167800612+pe66le@users.noreply.github.com>
2025-11-06 02:04:24 +0900

    2-11. URL 프리픽스(@RequestMapping) 정리

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,20 +1,21 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("questionList", questionService.getList());
         return "question_list";
     }
-}
 
-import org.springframework.web.bind.annotation.PathVariable;
-
-@GetMapping("/question/detail/{id}")
-public String detail(@PathVariable("id") Integer id, Model model) {
-    model.addAttribute("question", questionService.getQuestion(id));
-    return "question_detail";
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("question", questionService.getQuestion(id));
+        return "question_detail";
+    }
 }
